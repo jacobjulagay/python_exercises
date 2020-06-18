633d1654e4ceb4cfe3805be8bf27e5419dc61ee2
Jacob Julag-ay <jacobjulagay@gmail.com>
2020-06-17 22:49:13 -0700

    adding file

---
+++ basic.py
@@ -0,0 +1,27 @@
+# name = 'Alice' + 'Bob'
+# fivex = 'Alice' * 5
+# print(fivex)
+# print(name)
+
+# # Overwriting
+# spam = 'hello'
+# print(spam)
+# spam = 'goodbye'
+# print(spam)
+
+# Program says hello and asks for name
+
+# print('hello World!')
+# print("What is your name?")
+# myName = input() # sets myName to input. Returns input as a string
+# print("It is good to meet you, " + myName)
+# print('The length of your name is: ')
+# print(len(myName)) # len() passes in a string and output # of letters in string. Returns int
+# print('What is your age') # asking for age
+# myAge = input()
+# print('You will be ' + str(int(myAge) +1) + ' in a year ') # have to convert myAge to string because you can only concat str+str and int+int, not str+int
+
+# breakfast = 'I have eaten ' + str(99) + ' burritos.'
+# print(breakfast)
+# nums = 100 + int('10') + 50
+# print(nums)
